5b80c4e93b316820a2ab78b7e5a0a34824902e25
Kantaponn <78014536+Kantaponn@users.noreply.github.com>
2023-03-16 16:12:28 +0700

    edit api for product info page

--- models/service.model.js
+++ models/service.model.js
@@ -1,6 +1,10 @@
 import mongoose from "mongoose";
 import Product from "./schema/product.schema.js";
-
+import {
+  formatOpenHours,
+  checkOpenOrClose,
+  mapServiceTagIcon,
+} from "./schema/product.schema.js";
 const ServiceSchema = new mongoose.Schema(
   {
     ...Product,
@@ -21,10 +25,12 @@ ServiceSchema.methods.toAuthJSON = function () {
     name: this.name,
     status: this.status,
     province: this.province,
+    open_hours: formatOpenHours(this.open_hours),
   };
 };
 ServiceSchema.methods.toProductJSON = function () {
   return {
+    id: this._id,
     owner: this.owner,
     name: this.name,
     province: this.province,
@@ -38,8 +44,32 @@ ServiceSchema.methods.toProductJSON = function () {
     rating: this.rating,
     review_counts: this.review_counts,
     description: this.description || "",
-    open_hours: this.open_hours,
+    open_hours: formatOpenHours(this.open_hours),
   };
 };
+
+ServiceSchema.methods.toProductDetailJSON = function () {
+  return {
+    owner: this.owner,
+    name: this.name,
+    phones: this.phones,
+    social_networks: this.social_networks,
+    province: this.province,
+    amphure: this.amphure,
+    tambon: this.tambon,
+    status: this.status,
+    images: this.images,
+    location_description: this.location_description,
+    location: this.location,
+    tags: mapServiceTagIcon(this.serviceTags),
+    rating: this.rating,
+    review_counts: this.review_counts,
+    description: this.description || "",
+    open_hours: formatOpenHours(this.open_hours),
+    prices: this.prices,
+    open_status: checkOpenOrClose(this.open_hours, this.manual_close),
+  };
+};
+
 const Service = mongoose.model("Service", ServiceSchema);
 export default Service;
